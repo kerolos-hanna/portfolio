8e9183430a68bbe42b8cd6fb36390818012c1965
kerolos hanna saad <https://github.com/kerolos-hanna>
2020-05-23 16:03:03 +0200

    Updates

---
+++ precache-manifest.c84634556162fd03417fa94905666101.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79b6eaa8de1e91913d9375075028b6f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cccb41e066f16d182e43",
+    "url": "/portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4c9e4d0ccd5c52ec43a5",
+    "url": "/portfolio/static/css/main.c1a350be.chunk.css"
+  },
+  {
+    "revision": "cccb41e066f16d182e43",
+    "url": "/portfolio/static/js/2.608c044f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.608c044f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c9e4d0ccd5c52ec43a5",
+    "url": "/portfolio/static/js/main.953fdeaf.chunk.js"
+  },
+  {
+    "revision": "c93ab1f20ab9e5c7717b",
+    "url": "/portfolio/static/js/runtime-main.2a7b4a38.js"
+  },
+  {
+    "revision": "290205c61b78eedf2047a8877ff26913",
+    "url": "/portfolio/static/media/audio.290205c6.png"
+  },
+  {
+    "revision": "6a24c67b737bf178c6ed3c98701c8168",
+    "url": "/portfolio/static/media/ecom1.6a24c67b.png"
+  },
+  {
+    "revision": "c09411c6eb2882217759af05aa3fbdcf",
+    "url": "/portfolio/static/media/ecom2.c09411c6.png"
+  },
+  {
+    "revision": "3e7b78f36f3bd9ad4e16b512876adb36",
+    "url": "/portfolio/static/media/ecom3.3e7b78f3.png"
+  },
+  {
+    "revision": "864d482efc11ef4ff43658922903d8f8",
+    "url": "/portfolio/static/media/ecom4.864d482e.png"
+  },
+  {
+    "revision": "b2bc8a0abcdb0368ca7d51ce83d5459d",
+    "url": "/portfolio/static/media/me3.b2bc8a0a.jpg"
+  },
+  {
+    "revision": "cb1bd24d5b7679c281a1fc7c8caf066e",
+    "url": "/portfolio/static/media/pro1.cb1bd24d.jpg"
+  },
+  {
+    "revision": "5d6ebc181ee424a2c5bca59a397f5b0b",
+    "url": "/portfolio/static/media/pro2.5d6ebc18.jpg"
+  },
+  {
+    "revision": "b883662fdfa1a6c88f4403e7a745c290",
+    "url": "/portfolio/static/media/pro3.b883662f.jpg"
+  },
+  {
+    "revision": "b7ab5fe5abc98accfbbdd548501ee0e7",
+    "url": "/portfolio/static/media/web1.b7ab5fe5.png"
+  },
+  {
+    "revision": "62e9f73dc061addae27e6779520a91d1",
+    "url": "/portfolio/static/media/web2.62e9f73d.png"
+  },
+  {
+    "revision": "5f07c658eb43f858e80c764d09c2de25",
+    "url": "/portfolio/static/media/web3.5f07c658.png"
+  },
+  {
+    "revision": "e7bf065f54db5619f97bbce83dafaea5",
+    "url": "/portfolio/static/media/web4.e7bf065f.png"
+  }
+]);
