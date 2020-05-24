239fc44eba7d359ec84cee4d2c92c28da7c9b939
kerolos hanna saad <https://github.com/kerolos-hanna>
2020-05-24 02:15:57 +0200

    Updates

--- precache-manifest.e5e1a53622fee9db3a135ab8fa80f113.js
+++ precache-manifest.e5e1a53622fee9db3a135ab8fa80f113.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3ba07b1ff836a537e38a795899b5d602",
+    "revision": "7e2ea2818a58673736d67abd8fa05d7e",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "9d19ace37461b3ec3e55",
+    "revision": "95fae949b4ae7a95ea82",
     "url": "/portfolio/static/css/main.e3776b70.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.608c044f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9d19ace37461b3ec3e55",
-    "url": "/portfolio/static/js/main.bc2b2dcb.chunk.js"
+    "revision": "95fae949b4ae7a95ea82",
+    "url": "/portfolio/static/js/main.491dcd60.chunk.js"
   },
   {
     "revision": "c93ab1f20ab9e5c7717b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8028bfd298052f5e186a34555d29916d.js"
+  "/portfolio/precache-manifest.e5e1a53622fee9db3a135ab8fa80f113.js"
 );
 
 self.addEventListener('message', (event) => {
